c6fb24a9e2656e65942dd221f14453b6ec741b8b
natya2203 <nastya_2203@ukr.net>
2019-12-12 01:08:26 +0200

    Updates

---
+++ precache-manifest.5e602fca7ff9b5711aa0a51bc82e53b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b3cb0b87dcc933c8614b23177bcd83",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "d053b565df04b720a078",
+    "url": "/react_add-todo-form/static/css/main.7c7350c8.chunk.css"
+  },
+  {
+    "revision": "8c62e9e39566e04c3c1e",
+    "url": "/react_add-todo-form/static/js/2.263e448f.chunk.js"
+  },
+  {
+    "revision": "d053b565df04b720a078",
+    "url": "/react_add-todo-form/static/js/main.ee338357.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
